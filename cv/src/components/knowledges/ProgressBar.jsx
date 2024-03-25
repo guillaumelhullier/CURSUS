538a98d5abb544ffc63db9fc6c099933485d5e69
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-03-25 17:23:36 +0100

    progress

--- cv/src/components/knowledges/ProgressBar.jsx
+++ cv/src/components/knowledges/ProgressBar.jsx
@@ -1,47 +1,30 @@
-import React, { Component } from "react";
-import ProgressBar from "./ProgressBar";
+import React from "react";
 
-export default class Languages extends Component {
-  state = {
-    languages: [
-      { id: 1, value: "Javascript", xp: 1.8 },
-      { id: 2, value: "Css", xp: 2 },
-      /* { id: 3, value: "Php", xp: 0.7 },*/
-      { id: 3, value: "HTML", xp: 2 },
-      /* { id: 4, value: "Python", xp: 0.4 },*/
-    ],
-    frameworks: [
-      { id: 1, value: "React", xp: 1.4 },
-      /* { id: 2, value: "Bootstrap", xp: 2 },*/
-      { id: 3, value: "Sass", xp: 1.5 },
-      { id: 4, value: "Material UI", xp: 0.5 },
-    ],
-  };
-
-  render() {
-    let { languages, frameworks } = this.state;
-
-    return (
-      <div className="languagesFrameworks">
-        <ProgressBar
-          languages={languages}
-          className="languageDisplay"
-          title="languages"
-        />
-        <ProgressBar
-          languages={frameworks}
-          title="frameworks & bibliothèques"
-          className="frameworksDisplay"
-        />
+const ProgressBar = (props) => {
+  return (
+    <div className={props.className}>
+      <h3>{props.title}</h3>
+      <div className="years">
+        <span>Années d'expérience</span>
+        <span>1 an</span>
+        <span>2 ans</span>
       </div>
-    );
-  }
-}
 
-/*import React from "react";
+      <div>
+        {props.languages.map((item) => {
+          let xpYears = 2;
+          let progressBar = (item.xp / xpYears) * 100 + "%";
 
-const Languages = () => {
-  return <div>Languages et Frameworks</div>;
+          return (
+            <div key={item.id} className="languagesList">
+              <li>{item.value}</li>
+              <div className="progressBar" style={{ width: progressBar }}></div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default Languages;*/
+export default ProgressBar;
